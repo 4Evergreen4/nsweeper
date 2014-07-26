8e28f33f315da3b4ca55f13a3d3642096edc8418
4Evergreen4 <eljohnson@frontier.com>
2014-07-26 15:09:56 -0400

    Add curses startup and reset to the main function

--- game/main.py
+++ game/main.py
@@ -1,3 +1,18 @@
+import curses
+
 def main():
     '''Start game'''
-    print "started"
+
+    # Starts ncurses
+    stdscr = curses.initscr()
+
+    # Setup ncurses
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+
+    # Reset everything
+    curses.nocbreak()
+    curses.echo()
+    curses.curs_set(1)
+    curses.endwin()

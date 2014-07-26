42e7cce28b46073b66362d4e4c7c59e1ba3d257b
4Evergreen4 <eljohnson@frontier.com>
2014-07-26 15:28:48 -0400

    Add setup and reset stuff for stdscr

--- game/main.py
+++ game/main.py
@@ -10,8 +10,12 @@ def main():
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
+    stdscr.keypad(1)
+    stdscr.nodelay(1)
 
     # Reset everything
+    stdscr.keypad(0)
+    stdscr.nodelay(0)
     curses.nocbreak()
     curses.echo()
     curses.curs_set(1)

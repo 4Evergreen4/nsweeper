dd83271984656579841139b0d22a11a74299961d
4Evergreen4 <eljohnson@frontier.com>
2014-07-29 21:27:20 -0400

    Add the groundwork for buttons.

--- src/interface/window.py
+++ src/interface/window.py
@@ -15,3 +15,12 @@ def popup(x, y, w, h, title, text, screen):
     screen.addstr(y + 2, x, str(endstr))
 
     screen.refresh()
+
+class button(object):
+    def __init__(self, x, y, text):
+        self.text = text
+        self.x = x
+        self.y = y
+
+    def getstate():
+        pass

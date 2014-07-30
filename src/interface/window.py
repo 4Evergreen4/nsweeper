d6a74017180d49cc6dfcdf80ec200c180ef4fd70
4Evergreen4 <eljohnson@frontier.com>
2014-07-30 16:09:07 -0400

    Remove some unnecessary stuff.

--- src/interface/window.py
+++ src/interface/window.py
@@ -4,15 +4,15 @@ def popup(x, y, w, h, title, text, screen):
 
     topstr = str(title) + "-" * ((len(text) - len(title)) + 2)
 
-    screen.addstr(y, x, str(topstr))
+    screen.addstr(y, x, topstr)
 
     midstr = "|" + text + "|"
 
-    screen.addstr(y + 1, x, str(midstr))
+    screen.addstr(y + 1, x, midstr)
 
     endstr = "-" * (len(text) + 2)
 
-    screen.addstr(y + 2, x, str(endstr))
+    screen.addstr(y + 2, x, endstr)
 
     screen.refresh()
 
--- src/main.py
+++ src/main.py
@@ -3,34 +3,34 @@ import game
 
 def main():
     '''Start game'''
-    
+
     # Sets the starting coordinates
     win_x = 0
     win_y = 0
     # Sets the curses window size
     win_w = 80
     win_h = 24
-    
+
     # Starts ncurses
     stdscr = curses.initscr()
-    
+
     term_size = stdscr.getmaxyx()
-    
+
     if term_size[0] < 24 or term_size[1] < 80:
         curses.endwin()
         print "ERROR: Your console screen is smaller than 80x24."
         print "Please resize your window and try again."
-        return 1;
-    
+        return 1
+
     # Setup ncurses
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
     stdscr.keypad(1)
     stdscr.nodelay(1)
-    
+
     game.game(stdscr, win_x, win_y, win_w, win_h)
-    
+
     stop(stdscr)
 
 def stop(stdscr):
